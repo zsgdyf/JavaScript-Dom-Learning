717111c45c35eb8401aea1ca97c9e7f2f33d4bed
一切随缘 <34258580+zsgdyf@users.noreply.github.com>
2018-11-14 21:30:57 +0800

    update test

--- chp07/example.js
+++ chp07/example.js
@@ -6,6 +6,31 @@ function insertParagraph(text) {
 }
 
 window.onload = function() {
+    var para = document.createElement("p");
+    var txt1 = document.createTextNode("This is ");
+    para.appendChild(txt1);
+    var emph = document.createElement("em");
+    var txt2 = document.createTextNode("my");
+    emph.appendChild(txt2);
+    para.appendChild(emph);
+    var txt3 = document.createTextNode(" content");
+    para.appendChild(txt3);
+    var testdiv = document.getElementById("testdiv");
+    testdiv.appendChild(para);
+}
+
+/*
+window.onload = function() {
+    var para = document.createElement("p");
+    var testdiv = document.getElementById("testdiv");
+    testdiv.appendChild(para);
+    var txt = document.createTextNode("Hello World");
+    para.appendChild(txt);
+}
+*/
+/*
+window.onload = function() {
     var testdiv = document.getElementById("testdiv");
     alert(testdiv.innerHTML);
 }
+*/
