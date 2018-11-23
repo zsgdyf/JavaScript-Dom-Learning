be7aaee9fb801ce5f1fc272443f00ed6389e333f
一切随缘 <1273873106@qq.com>
2018-11-23 11:05:36 +0800

    chp11 init

---
+++ chp11/grayscale.js
@@ -0,0 +1,34 @@
+function convertToGs(img) {
+    img.color = img.src;
+    img.grayscale = createGsCanvas(img);
+    img.onmouseover = function() {
+        this.src = this.color;
+    }
+    img.onmouseout = function() {
+        this.src = this.grayscale;
+    }
+    img.onmouseout();
+}
+
+function createGsCanvas(img) {
+    var canvas = document.createElement("canvas");
+    canvas.width = img.width;
+    canvas.height = img.height;
+    var ctx = canvas.getContext("2d");
+    ctx.drawImage(img, 0, 0);
+    var c = ctx.getImageData(0, 0, img.width, img.height);
+    for (var i = 0; i < c.height; i++) {
+        for (var j = 0; j < c.width; j++) {
+            var x = (i * 4) * c.height + j * 4;
+            var r = c.data[x];
+            var g = c.data[x + 1];
+            var b = c.data[x + 2];
+            c.data[x] = c.data[x + 1] = c.data[x + 2] = (r + g + b) / 3;
+        }
+    }
+    ctx.putImageData(c, 0, 0, 0, 0, c.width, c.height);
+    return canvas.toDataURL();
+}
+window.onload = function() {
+    convertToGs(document.getElementById("avatar"));
+}
